afc0c0d7fcc9048e52c798055fe4b2ff03054888
Hugo <hugome2307@gmail.com>
2023-02-12 17:40:45 +0100

    h

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+        LogicaNegocio logicaNegocio = new LogicaNegocio();
+        logicaNegocio.ejecutarAccionesPrototipo();
+    }
 
-    LogicaNegocio logicaNegocio = new LogicaNegocio();
 }
